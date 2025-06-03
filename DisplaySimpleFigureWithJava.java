a129f97412970387ff0bc0b164f51c9330155fb5
reza-sadeqi-git <hjrz1352@gmail.com>
2025-06-03 10:50:30 +0330

    function stars added

--- DisplaySimpleFigureWithJava.java
+++ DisplaySimpleFigureWithJava.java
@@ -10,7 +10,11 @@ public class DisplaySimpleFigureWithJava {
         System.out.println("* | | | | | *");
     }
 }
-
+public static void stars() {
+    System.out.println("    *****    ");
+    System.out.println("  *********  ");
+    System.out.println("*************");
+}
 
 
 
